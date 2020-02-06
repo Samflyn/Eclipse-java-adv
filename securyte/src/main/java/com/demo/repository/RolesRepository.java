d6134f2827efc9582f289ee773c96585bed90fb8
saiakhil.sadineni <saiakhil.sadineni@sailotech.com>
2020-02-06 17:29:45 +0530

    Clean Securyte

--- securyte/src/main/java/com/demo/repository/RolesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.bean.Roles;
-
-public interface RolesRepository extends JpaRepository<Roles, Long> {
-
-}
