700e6ee6181118af19c6e91cc00da9d7ad003caf
Samflyn <akhil.sai701@gmail.com>
2020-02-09 19:10:24 +0530

    Added SSL and Https redirection

---
+++ securyte/src/main/java/com/demo/config/SSLRedirectionConfig.java
@@ -0,0 +1,41 @@
+package com.demo.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SSLRedirectionConfig {
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		// Enable SSL Traffic
+		TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection securityCollection = new SecurityCollection();
+				securityCollection.addPattern("/*");
+				securityConstraint.addCollection(securityCollection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcatServletWebServerFactory.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+		return tomcatServletWebServerFactory;
+	}
+
+	private Connector httpToHttpsRedirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}
+
+}
